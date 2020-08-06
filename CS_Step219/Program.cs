b8215758aa68c57def9cd70f646ddcce7acf8325
Gavin Kimberlin <guviner@gmail.com>
2020-08-06 05:22:18 -0700

    Create a program using streamreader and streamwriter.

--- CS_Step219/Program.cs
+++ CS_Step219/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace CS_Step219
 {
@@ -10,6 +11,17 @@ namespace CS_Step219
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter a number");
+            string number = Convert.ToString(Console.ReadLine());
+            using (StreamWriter file = new StreamWriter(@"C:\Users\guvin\source\repos\CS_Step219\log.txt", true))
+            {
+                file.WriteLine(number);
+            }
+            using (StreamReader file = new StreamReader(@"C:\Users\guvin\source\repos\CS_Step219\log.txt"))
+            {
+                Console.WriteLine("\n\nThe numbers currently logged to the text file are:\n" + file.ReadToEnd());
+            }
+            Console.ReadLine();
         }
     }
 }
